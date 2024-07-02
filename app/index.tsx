cda41106e1acc289a0bcf463e6c11210113f3f85
Paul <paul.thiebault@epitech.eu>
2024-07-02 17:09:06 +0200

    add: Text & Title components

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,16 @@ import { Stack, Link } from 'expo-router';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
+import Text from '~/components/Text/Text';
+import Title from '~/components/Text/Title';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home', headerShown: false }} />
       <Container>
+        <Title className="text-4xl">Welcome to Fundamental!</Title>
+        <Text className="text-lg">This is Fundamental</Text>
         <ScreenContent path="app/index.tsx" title="Home" />
         <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
           <Button title="Show Details" />
---
+++ components/Text/Title.tsx
@@ -0,0 +1,33 @@
+import {
+  useFonts,
+  DMSerifText_400Regular,
+  DMSerifText_400Regular_Italic,
+} from '@expo-google-fonts/dm-serif-text';
+import { Text } from 'react-native';
+
+interface TitleProps {
+  children: React.ReactNode;
+  className?: string;
+  italic?: boolean;
+}
+
+const Title = ({ children, className, italic, ...props }: TitleProps) => {
+  const [fontsLoaded] = useFonts({
+    DMSerifText_400Regular,
+    DMSerifText_400Regular_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const font = italic ? 'DMSerifText_400Regular_Italic' : 'DMSerifText_400Regular';
+
+  return (
+    <Text className={`${className}`} style={{ fontFamily: font }} {...props}>
+      {children}
+    </Text>
+  );
+};
+
+export default Title;
