5796135a80dad0676f951a6354e1444cd7fff514
Paul <paul.thiebault@epitech.eu>
2024-07-03 17:15:28 +0200

    fix: Container outer margin

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -7,7 +7,7 @@ const fundy = require('../assets/fundy.png');
 
 const TitleBar = () => {
   return (
-    <View className="h-50 flex-row items-center gap-2 bg-background px-4 py-6">
+    <View className="h-50 flex-row items-center gap-2 py-4">
       <Icon name="menu" className="text-text" size={36} />
       <Title className="mt-2 text-4xl text-text">Home</Title>
       <Image
--- components/Wrappers/Container.tsx
+++ components/Wrappers/Container.tsx
@@ -1,9 +1,14 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <SafeAreaView className={styles.container}>{children}</SafeAreaView>;
+  return (
+    <View className={styles.outerMargin}>
+      <SafeAreaView className={styles.container}>{children}</SafeAreaView>
+    </View>
+  );
 };
 
 const styles = {
-  container: 'flex flex-1 p-6 bg-background h-screen',
+  outerMargin: 'flex flex-1 bg-background',
+  container: 'flex flex-1 m-6 bg-background h-screen',
 };
