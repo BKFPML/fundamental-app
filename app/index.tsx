3375d57707907cd79866ec6f3a7f77e6a777ea2a
Dimerci <maxime.lambert@epitech.eu>
2024-07-04 11:07:03 +0200

    add: basic privy implentation. PrivyProvider doesn't work

--- app/index.tsx
+++ app/index.tsx
@@ -6,17 +6,24 @@ import { ScreenContent } from '~/components/ScreenContent';
 import Text from '~/components/Text/Text';
 import Title from '~/components/Text/Title';
 
+import 'fast-text-encoding';
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
+import {PrivyProvider} from '@privy-io/expo';
+
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home', headerShown: false }} />
       <Container>
+      <PrivyProvider appId={'clxd5oc5m007jrpv8y8clt6z7'}>
         <Title className="text-4xl">Welcome to Fundamental!</Title>
         <Text className="text-lg">This is Fundamental</Text>
         <ScreenContent path="app/index.tsx" title="Home" />
         <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
           <Button title="Show Details" />
         </Link>
+        </PrivyProvider>
       </Container>
     </>
   );
