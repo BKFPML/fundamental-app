22bed53663f22508dae161515ff23d6e9f362bd3
bendsp <benjy.desprets@gmail.com>
2024-10-17 12:24:11 -0400

    fix: wrong indent level

--- components/Services/alchemyService.ts
+++ components/Services/alchemyService.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
+const ALCHEMY_API_KEY = process.env.EXPO_PUBLIC_ALCHEMY_API_KEY
 
 const getEthBalance = async (address: string) => {
     const network = "base"
-    const ALCHEMY_API_KEY = process.env.EXPO_PUBLIC_ALCHEMY_API_KEY
     const Url = `https://${network}-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
 
     const data = {
