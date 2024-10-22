4faf7423361cdeefbeb07f278eac56b37f0a4bb7
Felixsta123 <Felixsta123@users.noreply.github.com>
2024-10-22 15:23:28 +0000

    deploy: 06b149c82115f0708b6e9ee7e7c12b1a7fbe7012

--- index.ts
+++
@@ -1,8 +0,0 @@
-// file: index.ts
-
-// polyfills
-import 'fast-text-encoding';
-import 'react-native-get-random-values';
-import '@ethersproject/shims';
-// import '@thirdweb-dev/react-native-adapter';
-import 'expo-router/entry';
